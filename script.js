30233aca20888e88eb41c39972475d877158ded6
ezeProyectos <eze.m.belmonte@gmail.com>
2024-10-18 22:20:58 -0300

    Calculadora básica realizada con JavaScript, HTML y CSS

--- script.js
+++ script.js
@@ -11,9 +11,30 @@ function borrar() {
 
 // Esta acción toma el valor de la pantalla y ejecuta los cálculos.
 function calcular() {
-    const valorPantalla = document.getElementById('pantalla').value
+    /* En valorPantalla se guarda el String de la pantalla y el .replace(/\^/g, '**') 
+    hace que todos los ^ (que representa el exponente) se conviertan en ** ya que esa
+    es la expresion correcta para JavaScript para calcular el exponente. 
+    En .replace(/√(\d+)/g, 'Math.sqrt($1)'):
+      - La expresión "/√(\d+)/g" busca el símbolo √ seguido de uno o más dígitos (\d+) lo que permite extraer el número que sigue al símbolo.
+      - En el segundo argumento de replace,"$1" representa el número capturado, que se inserta dentro de Math.sqrt().
+    */
+    let valorPantalla = document.getElementById('pantalla').value.replace(/\^/g, '**').replace(/√(\d+)/g, 'Math.sqrt($1)');
     // eval es un método propio de JavaScript que toma un string y lo calcula.
     const resultado = eval(valorPantalla)
     // Le pasamos el resultado a la pantalla para que lo muestre.
     document.getElementById('pantalla').value = resultado
 }
+
+
+/*
+function calcular() {
+    /* En valorPantalla se guarda el String de la pantalla y el .replace(/\^/g, '**') 
+    hace que todos los ^ (que representa el exponente) se conviertan en ** ya que esa
+    es la expresion correcta para JavaScript para calcular el exponente. */
+ //   let valorPantalla = document.getElementById('pantalla').value.replace(/\^/g, '**');
+    // eval es un método propio de JavaScript que toma un string y lo calcula.
+//    const resultado = eval(valorPantalla)
+    // Le pasamos el resultado a la pantalla para que lo muestre.
+ //   document.getElementById('pantalla').value = resultado
+//}
+
